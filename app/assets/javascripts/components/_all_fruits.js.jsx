44d321eae920702ee880de222f5264755523169f
DataNick <DataNick@users.noreply.github.com>
2018-07-13 17:48:30 -0400

    Refactor AllFruits to be stateless; add Body component to Main

--- app/assets/javascripts/components/_all_fruits.js.jsx
+++ app/assets/javascripts/components/_all_fruits.js.jsx
@@ -1,19 +1,5 @@
-class AllFruits extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      fruits: []
-    };
-  }
-
-  componentDidMount() {
-    fetch('/api/v1/fruits')
-      .then((response) => { return response.json() })
-      .then((data) => {this.setState({ fruits: data }) });
-  }
-
-  render(){
-      var fruits = this.state.fruits.map((fruit) => {
+const AllFruits = (props) => {
+  let fruits = props.fruits.map((fruit) => {
         return(
           <li key={fruit.id}>
             <h1>{fruit.name}</h1>
@@ -21,30 +7,48 @@ class AllFruits extends React.Component {
           </li>
         )
       })
-  return(
-        <ul>
-          {fruits}
-        </ul>
-      )
-    }
-  // render(){
-  //   let fruits = this.state.fruits.map((fruit) => {
-  //     return(
-  //       <div>
-  //         <h1>To do: List of all fruits</h1>
-  //         <ul>
-  //           { this.state.fruits.map((name, index) => {
-  //             return <li key={index}>{name}</li>
-  //           })}
-  //         </ul>
-  //       </div>
-  //     )
-  //   })
-  //   return(
-  //     <div>
-  //       {fruit}
-  //     </div>
-  //     )
-  // }
+     return(
+          <ul>
+            {fruits}
+          </ul>
+        )
 }
 
+// class AllFruits extends React.Component {
+
+//   render(){
+//       var fruits = this.state.fruits.map((fruit) => {
+//         return(
+//           <li key={fruit.id}>
+//             <h1>{fruit.name}</h1>
+//             <p>{fruit.description}</p>
+//           </li>
+//         )
+//       })
+//   return(
+//         <ul>
+//           {fruits}
+//         </ul>
+//       )
+//     }
+//   // render(){
+//   //   let fruits = this.state.fruits.map((fruit) => {
+//   //     return(
+//   //       <div>
+//   //         <h1>To do: List of all fruits</h1>
+//   //         <ul>
+//   //           { this.state.fruits.map((name, index) => {
+//   //             return <li key={index}>{name}</li>
+//   //           })}
+//   //         </ul>
+//   //       </div>
+//   //     )
+//   //   })
+//   //   return(
+//   //     <div>
+//   //       {fruit}
+//   //     </div>
+//   //     )
+//   // }
+// }
+//Modify to be stateless/presentation-only component
